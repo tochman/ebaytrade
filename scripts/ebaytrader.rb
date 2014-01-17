e3550826cc7ceebf367a36c66b8f8f6889eebf13
Thomas Ochman <thomas@nocebo.se>
2014-01-17 10:38:02 +0100

    added API classes and methods

--- scripts/ebaytrader.rb
+++ scripts/ebaytrader.rb
@@ -6,6 +6,7 @@
 # $ irb
 # > load './ebaytrader.rb'
 # > get_listing_price (or any method you want to call)
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require 'rubygems'
 require 'nokogiri'
@@ -14,6 +15,9 @@ require 'net/http'
 require 'uri'
 require 'active_support/all'
 require 'active_support/xml_mini/nokogiri'
+require '../lib/eBayAPI'
+gem 'soap2r'
+
 
 
 def get_listing_price
@@ -23,76 +27,54 @@ def get_listing_price
 
   @parsed.each do |product|
     @product = product
-    mapping(product)
+    create_listing(product)
   end
 
-  product_xml
-  create_listing
-end
-
-def product_xml
-  @my_xml = (@mapping).to_xml( :root => 'Item',
-                               :skip_instruct => true,
-                               :indent => 0,
-                               :skip_types => true,
-                               :save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip
-  #@my_xml = (@mapping).to_xml(:root=>'Item', :skip_instruct => true, :indent => 2, :skip_types => true)
-  #puts @my_xml
-end
-
-def mapping(product)
-  @mapping = Array.new
-  @mapping << {:Title => product[:name],
-              :Description => product[:description],
-              :Location => 'US',
-              :StartPrice => product[:price],
-              :Quantity => '1',
-              :ListingDuration => 'Days_7',
-              :Country => 'US',
-              :Currency => 'USD',
-              :PaymentMethods => ['PayPal', 'VisaMC', 'PersonalCheck'],
-              :PayPalEmailAddress => 'merchant@merchant.xx'
-               }
-
+  #product_xml
+  #create_listing
 end
 
 # AddItemRequest
-def create_listing
-  #XML API Sandbox Gateway URI  https://api.sandbox.ebay.com/ws/api.dll
-  #XML API Production Gateway URI https://api.ebay.com/ws/api.dll
-
+def create_listing(product)
 
   load('myCredentials.rb')
+  eBay = EBay::API.new($authToken, $devId, $appId, $certId, :sandbox => true)
+
+  resp = eBay.AddItem(:Item => EBay.Item({ :PrimaryCategory => EBay.Category({ :CategoryID => 57882 }),
+                                           :Title => product[:name],
+                                           :Description => product[:description],
+                                           :ConditionID => '1500',
+                                           :ConditionDescription => 'Good',
+                                           :Location => 'US',
+                                           :StartPrice => '1',
+                                           :BuyItNowPrice => product[:price],
+                                           :Quantity => 1,
+                                           :DispatchTimeMax => '3',
+                                           :ListingDuration => 'Days_7',
+                                           :Country => 'US',
+                                           :Currency => 'USD',
+                                           :PaymentMethods => 'VisaMC',
+                                           #:PayPalEmailAddress => 'magicalbookseller@yahoo.com',
+                                           :ShippingDetails => EBay.ShippingDetails(
+                                               :ShippingServiceOptions => [
+                                                   EBay.ShippingServiceOptions(
+                                                       :ShippingService => ShippingServiceCodeType::USPSPriority,
+                                                       :ShippingServiceCost => '0.0',
+                                                       :ShippingServiceAdditionalCost => '0.0'),
+                                                   EBay.ShippingServiceOptions(
+                                                       :ShippingService => ShippingServiceCodeType::USPSPriorityFlatRateBox,
+                                                       :ShippingServiceCost => '7.0',
+                                                       :ShippingServiceAdditionalCost => '0.0')]),
+                                          :ReturnPolicy => EBay.ReturnPolicy(
+                                              :ReturnsAcceptedOption => 'ReturnsAccepted',
+                                              :Description => 'If you are not satisfied, return the item for refund.',
+                                              :RefundOption => 'MoneyBack',
+                                              :ReturnsWithinOption => 'Days_30'
+                                          )}))
+
+  puts "New Item #" + resp.itemID + " added."
+  puts "You spent:\n"
 
 
-  url = URI.parse('https://api.sandbox.ebay.com/ws/api.dll')
-
-  req = Net::HTTP::Post.new(url.path)
-  req.add_field("X-EBAY-API-COMPATIBILITY-LEVEL", "853")
-  req.add_field("X-EBAY-API-DEV-NAME", $devId)
-  req.add_field("X-EBAY-API-APP-NAME", $appId)
-  req.add_field("X-EBAY-API-CERT-NAME", $certId)
-  req.add_field("X-EBAY-API-SITEID", "0")  # '0' is US site
-  req.add_field("X-EBAY-API-CALL-NAME", "AddItem")
-
-  req.body = '<?xml version="1.0" encoding="utf-8"?>'+
-      '<AddItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">'+
-      '<RequesterCredentials>'+
-      "<eBayAuthToken>#{$authToken}</eBayAuthToken>"+
-      '</RequesterCredentials>'+
-      @my_xml +
-      '</AddItemRequest>'
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = 0
-  res = http.start do |http_runner|
-    http_runner.request(req)
-  end
-  puts req.body
-  puts "Responce:"
-  puts res.body
-
-  #puts "New Item #" + res.itemID + " added."
 end
 
